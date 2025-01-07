50d0d66af80f2f4d7b6250f8078aeb70ac67bf73
Ke.O <74878728+kenawak@users.noreply.github.com>
2025-01-07 12:38:57 -0800

    update vercel.json

--- src/api/api.js
+++
@@ -1,6 +0,0 @@
-import axios from axios
-
-export const createProject = async (project) => {
-    const response = await axios.post('http://localhost:1337/projects', project);
-    return response.data;
-}
