52c530f0f6080e9502ff83eb3804fa00e5417990
Ke.O <74878728+kenawak@users.noreply.github.com>
2025-03-06 00:42:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProjectForm from './components/ProjectForm';
 import Welcome from './components/Welcome';
+// import ThemeToggle from './components/ThemeToggle'; // Commented out as it's not used
 import './App.css';
 
 // const API_URL = process.env.REACT_APP_API_URL; // Commented out as it's not used
 
 
 function App() {
-  const [user, setUser] = useState(null);
+  // const [darkMode, setDarkMode] = useState(false);
 
-    useEffect(() => {
-        // Ensure the Telegram Web App is ready
-        if (window.Telegram) {
-            window.Telegram.WebApp.ready();
+  // const toggleDarkMode = () => { // Commented out as it's not used
+  //   setDarkMode(!darkMode);
+  //   if (darkMode) {
+  //     document.documentElement.classList.remove('dark');
+  //   } else {
+  //     document.documentElement.classList.add('dark');
+  //   }
+  // };
 
-            // Access the user information
-            const obj = window.Telegram.WebApp.initDataUnsafe;
-            console.log("tg-object:", obj);
-            const user_data = window.Telegram.WebApp.initDataUnsafe.user;
-            console.log(user)
-        // Get the user_id
-        if (user_data) {
-            setUser(user_data);
-            console.log('User ID:', user_data);
-        }else{
-            console.log('No user ID');
-        }
-      }
-    }, []);
   return (
     <Router>
       <div className='font-mono'>
-        
         <Routes>
           <Route path="/" element={<Welcome />} />
-          <Route path="/project-form" element={<ProjectForm user={user} />} />
+          <Route path="/project-form" element={<ProjectForm />} />
         </Routes>
       </div>
     </Router>
