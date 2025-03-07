78e8c8c28eafeb724f645a687ddb6e1bfe45ee52
kenawak <kenawakibsa95@gmail.com>
2025-03-08 01:38:33 +0300

    update welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -14,12 +14,12 @@ const Welcome = () => {
 
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-br from-blue-500 to-purple-600 dark:from-gray-800 dark:to-gray-900 text-white">
-        <div className="flex justify-center items-center">
-    <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12"></div>
-  </div>
+      <div className="flex justify-center items-center">
+        <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12"></div>
+      </div>
       <div className="text-center px-6">
         <h1 className="text-4xl md:text-6xl font-bold mb-6">Welcome to Home Of Projects</h1>
-        <p className="text-lg md:text-xl mb-10">Boost your audiance & reachability through broadcasting to the Channel</p>
+        <p className="text-lg md:text-xl mb-10">Boost your audience & reachability through broadcasting to the Channel</p>
       </div>
     </div>
   );
